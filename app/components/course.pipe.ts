182847504f078432ec00e8ce371399199ed75e17
Philipp Daniels <philipp.daniels@gmail.com>
2016-05-30 02:39:44 +0200

    #8 Add filter input for courses

---
+++ app/components/course.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe } from '@angular/core';
+
+@Pipe({
+    name: 'courseFilter',
+    pure: false
+})
+export class CoursePipe {
+    transform(items: any, value: String = ""): any {
+        if (!items) {
+            return;
+        } else {
+            return items.filter(item => item.name.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) >= 0);
+        }
+    }
+}
--- app/components/overview.component.ts
+++ app/components/overview.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 import { ConnectionBackend, Response } from '@angular/http';
 
 import { RestService } from '../rest.service';
+import { CoursePipe } from './course.pipe';
 
 @Component({
     selector: 'cloud-app',
     templateUrl: '/tpl/overview.html',
     directives: [ROUTER_DIRECTIVES],
-    providers: [RestService, ConnectionBackend]
+    providers: [RestService, ConnectionBackend],
+    pipes: [CoursePipe]
 })
 export class OverviewComponent {
+    @ViewChild('inputCourseFilter') inputElementRef;
     courses: Array;
+    filterCourse: String;
 
-    constructor(rest: RestService) {
+    constructor(renderer: Renderer, rest: RestService) {
         rest.getCourses().subscribe((res: Response) => {
             this.courses = res.json();
+            renderer.invokeElementMethod(this.inputElementRef.nativeElement, 'focus', []);
         });
     }
+
 }

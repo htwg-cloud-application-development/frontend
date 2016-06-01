ed08683453e187d6a97510d4da36950ca3594ef5
Philipp Daniels <philipp.daniels@gmail.com>
2016-06-01 19:36:05 +0200

    #8 Add angular2-modal

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,18 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-
-import { RestService } from './rest.service';
 import { COMMON_DIRECTIVES } from '@angular/common';
 import { ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
 import { HTTP_PROVIDERS, HTTP_BINDINGS } from '@angular/http';
+import { MODAL_BROWSER_PROVIDERS } from 'angular2-modal/platform-browser';
 
-bootstrap(AppComponent, [RestService, COMMON_DIRECTIVES, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, HTTP_PROVIDERS, HTTP_BINDINGS]);
+import { AppComponent } from './app.component';
+import { RestService } from './rest.service';
+
+bootstrap(AppComponent, [
+    COMMON_DIRECTIVES,
+    ROUTER_DIRECTIVES,
+    ROUTER_PROVIDERS,
+    HTTP_PROVIDERS,
+    HTTP_BINDINGS,
+    MODAL_BROWSER_PROVIDERS,
+    RestService
+]);
--- systemjs.config.js
+++ systemjs.config.js
@@ -8,13 +8,17 @@
     'app':                        'app', // 'dist',
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+    'rxjs':                       'node_modules/rxjs',
+    'angular2-modal':             'node_modules/angular2-modal'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'angular2-modal':                   { main: 'index.js', defaultExtension: 'js' },
+    'angular2-modal/platform-browser':  { main: 'index.js', defaultExtension: 'js' },
+    'angular2-modal/plugins/bootstrap': { main: 'index.js', defaultExtension: 'js' }
   };
   var ngPackageNames = [
     'common',
@@ -25,7 +29,7 @@
     'platform-browser-dynamic',
     'router',
     'router-deprecated',
-    'upgrade',
+    'upgrade'
   ];
   // Add package entries for angular packages
   ngPackageNames.forEach(function(pkgName) {
@@ -34,6 +38,6 @@
   var config = {
     map: map,
     packages: packages
-  }
+  };
   System.config(config);
 })(this);

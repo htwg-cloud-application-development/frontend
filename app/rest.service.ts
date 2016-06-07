a1740837fa472a7ead3fd33bdd788ca96491cd75
Philipp Daniels <philipp.daniels@gmail.com>
2016-06-07 11:06:17 +0200

    #11 Use new config constant at rest.service module

--- app/rest.service.ts
+++ app/rest.service.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
+import { CONFIG } from './config';
+
 @Injectable()
 export class RestService {
-    server = '';
 
     constructor(private http: Http) {}
 
     private httpGet(path: String): any {
-        return this.http.get(this.server + path + '.json');
+        return this.http.get(CONFIG.rest_api + path + '.json');
     }
 
     getCourses(): any {
-        return this.httpGet('/apiv1/governance-service/courses');
+        return this.httpGet('/governance-service/courses');
     }
 }

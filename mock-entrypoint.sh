b9de8d9e38872fb212a411ae74394c8e9c7305bb
Philipp Daniels <philipp.daniels@gmail.com>
2016-06-07 17:46:35 +0200

    #9 Extend mock-service to ignore query string in urls

--- mock-entrypoint.sh
+++ mock-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export PATH=$PATH:$PWD/node_modules/.bin
 npm install --unsafe-perm
-exec ws -d apiv1 --verbose --rewrite '/* -> $1.json'
+exec ws -d apiv1 --verbose --rewrite '/*\?* -> $1.json' --rewrite '/* -> $1.json'

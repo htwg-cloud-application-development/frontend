99f1a7a6e12a19cd3d042af205f0fa2dc7043dc2
Philipp Daniels <philipp.daniels@gmail.com>
2016-06-13 18:33:03 +0200

    #18 Enable angular2 production mode via config flag

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,17 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
 import { COMMON_DIRECTIVES } from '@angular/common';
 import { ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
 import { HTTP_PROVIDERS, HTTP_BINDINGS } from '@angular/http';
 import { MODAL_BROWSER_PROVIDERS } from 'angular2-modal/platform-browser';
 
+import { CONFIG } from './config';
 import { AppComponent } from './app.component';
 import { RestService } from './rest.service';
+
+if (CONFIG.mode == "prod") {
+    enableProdMode();
+}
 
 bootstrap(AppComponent, [
     COMMON_DIRECTIVES,

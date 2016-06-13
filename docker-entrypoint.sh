23c77fd5b5d95b7c3e519276803dba90d191cf4f
Philipp Daniels <philipp.daniels@gmail.com>
2016-06-13 19:48:00 +0200

    #18 Extend docker-entrypoint.sh to handle PROD and DEV mode

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 npm install --unsafe-perm
-exec npm start
+
+if [ "$NODE_ENV" = "production" ]; then
+  echo "Starting PRODUCTION mode"
+  exec npm start
+else
+  echo "Starting DEV mode"
+  npm run tsc
+  exec ./node_modules/.bin/concurrently "npm run tsc:w" "npm run lite"
+fi

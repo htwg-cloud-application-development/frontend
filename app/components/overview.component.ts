99ae064b8cb04bdf2554e07cb8081c0127a3a3a2
Philipp Daniels <philipp.daniels@gmail.com>
2016-06-24 11:57:18 +0200

    Move custom-modal into overview.component.ts

--- app/components/overview.component.ts
+++ app/components/overview.component.ts
@@ -1,15 +1,81 @@
 import { Component, ViewChild, Renderer } from '@angular/core';
 import { ConnectionBackend, Response } from '@angular/http';
-import { Modal } from 'angular2-modal/plugins/bootstrap';
+import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
+import { DialogRef, ModalComponent } from 'angular2-modal';
 
 import { RestService } from '../rest.service';
 import { CoursePipe } from './course.pipe';
 import { ShortenRepoPipe } from './shorten-repo.pipe';
-import { ModalWindow, ModalContext } from './custom-modal';
 import { CourseModalWindow, CourseModalContext } from './validation-course.modal';
 import { HeaderDirective, EntriesDirective } from './navbar.directive';
+import { ValuesPipe } from './values.pipe';
+import { KeysPipe } from './keys.pipe';
 
 declare var $:any;
+
+export class ModalContext extends BSModalContext {
+    public pmd;
+    public checkstyle;
+
+    constructor(public group) {
+        super();
+        this.size = 'lg';
+    }
+}
+
+@Component({
+    selector: 'modal-content',
+    templateUrl: '/tpl/modal.html',
+    providers: [RestService, ConnectionBackend],
+    pipes: [ValuesPipe, KeysPipe]
+})
+export class ModalWindow implements ModalComponent<ModalContext> {
+    context: ModalContext;
+    loaderCheckstyle = true;
+    loaderPmd = true;
+
+    constructor(public dialog: DialogRef<ModalContext>, private rest: RestService) {
+        this.context = dialog.context;
+        this.loadPmd(rest);
+        this.loadCheckstyle(rest);
+    }
+
+    loadPmd(rest: RestService) {
+        rest.getPmdResult(this.context.group.userId).subscribe(
+            (res: Response) => {
+                this.loaderPmd = false;
+                this.context.pmd = res.json();
+            },
+            (err: Response) => {
+                this.loaderPmd = false;
+            }
+        );
+    }
+
+    loadCheckstyle(rest: RestService) {
+        rest.getCheckstyleResult(this.context.group.userId).subscribe(
+            (res: Response) => {
+                this.loaderCheckstyle = false;
+                this.context.checkstyle = res.json();
+            },
+            (err: Response) => {
+                this.loaderCheckstyle = false;
+            }
+        );
+    }
+
+    beforeDismiss() {
+        return true;
+    }
+
+    beforeClose() {
+        return true;
+    }
+
+    onXClick() {
+        this.dialog.destroy();
+    }
+}
 
 @Component({
     selector: 'cloud-app',
